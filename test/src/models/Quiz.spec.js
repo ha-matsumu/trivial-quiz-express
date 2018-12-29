594b3f861138df1ff44adba3a921df7aa34ba696
ha-matsumu <ha.matsumu@gmail.com>
2018-12-29 22:05:34 +0900

    modified Quiz.spec.js

--- test/src/models/Quiz.spec.js
+++ test/src/models/Quiz.spec.js
@@ -10,14 +10,33 @@ const quizTestData = {
   type: "boolean",
 };
 
-describe("Quizクラス", () => {
-  it("プロパティ確認", () => {
-    const quiz = new Quiz(quizTestData);
-    assert.equal(quiz.category, "Entertainment: Video Games");
-    assert.equal(quiz.correctAnswer, "True");
-    assert.equal(quiz.difficulty, "medium");
-    assert.equal(Array.isArray(quiz.incorrectAnswers), true);
-    assert.equal(quiz.question, "Amazon acquired Twitch in August 2014 for $970 million dollars.");
-    assert.equal(quiz.type, "boolean");
+const quiz = new Quiz(quizTestData);
+describe("Quizクラスのテスト", () => {
+  it("categoryの型チェック", () => {
+    assert.equal(typeof quiz.category, "string", "string型ではありません。");
+  });
+
+  it("correctAnswerの型チェック", () => {
+    assert.equal(typeof quiz.correctAnswer, "string", "string型ではありません。");
+  });
+
+  it("difficultyの型チェック", () => {
+    assert.equal(typeof quiz.difficulty, "string", "string型ではありません。");
+  });
+
+  it("incorrectAnswersの型チェック", () => {
+    assert.equal(Array.isArray(quiz.incorrectAnswers), true, "配列ではありません。");
+  });
+
+  it("incorrectAnswersに格納されている値の型チェック", () => {
+    assert.equal(typeof quiz.incorrectAnswers[0], "string", "string型ではありません。");
+  });
+
+  it("questionの型チェック", () => {
+    assert.equal(typeof quiz.question, "string", "string型ではありません。");
+  });
+
+  it("typeの型チェック", () => {
+    assert.equal(typeof quiz.type, "string", "string型ではありません。");
   });
 });
