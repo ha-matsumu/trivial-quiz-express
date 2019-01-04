96a4f0a8f416a57d24a794c6510261b1cfd374a0
ha-matsumu <ha.matsumu@gmail.com>
2019-01-04 23:25:40 +0900

    modified QuizFetcher.spec.js

--- test/src/models/QuizFetcher.spec.js
+++ test/src/models/QuizFetcher.spec.js
@@ -1,5 +1,6 @@
 const assert = require("power-assert");
 const QuizFetcher = require("../../../src/models/QuizFetcher");
+const Quiz = require("../../../src/models/Quiz");
 
 describe("QuizFetcherクラスのテスト", () => {
   it("getQuizDataListの動作チェック", () => {
@@ -9,43 +10,11 @@ describe("QuizFetcherクラスのテスト", () => {
         true,
         "quizInstancesは配列ではありません。"
       );
-      quizInstances.forEach(quiz => {
+      quizInstances.forEach(quizInstance => {
         assert.equal(
-          typeof quiz.category,
-          "string",
-          "categoryはstring型ではありません。"
-        );
-        assert.equal(
-          typeof quiz.correctAnswer,
-          "string",
-          "correctAnswerはstring型ではありません。"
-        );
-        assert.equal(
-          typeof quiz.difficulty,
-          "string",
-          "difficultyはstring型ではありません。"
-        );
-        assert.equal(
-          Array.isArray(quiz.incorrectAnswers),
+          quizInstance instanceof Quiz,
           true,
-          "incorrectAnswersは配列ではありません。"
-        );
-        quiz.incorrectAnswers.forEach(incorrectAnswer => {
-          assert.equal(
-            typeof incorrectAnswer,
-            "string",
-            "incorrectAnswersに格納されている値は文字列ではありません。"
-          );
-        });
-        assert.equal(
-          typeof quiz.question,
-          "string",
-          "questionはstring型ではありません。"
-        );
-        assert.equal(
-          typeof quiz.type,
-          "string",
-          "typeはstring型ではありません。"
+          "Quizクラスのインスタンスではありません。"
         );
       });
     });
