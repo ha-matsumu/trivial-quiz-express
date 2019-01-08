4c5a39d501187d865b57251d7ea58c10185a8213
ha-matsumu <ha.matsumu@gmail.com>
2019-01-08 23:36:41 +0900

    modified routing

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ const app = express();
 const quizRouter = require("./routes/quizRouter");
 const router = require("./routes/router");
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
+
+app.set('views', __dirname + '/views');
 app.set("view engine", "ejs");
 
 app.use("/", router);
-
 app.use("/api", quizRouter);
 
 app.listen("3000", () => {
