db65d6715b4bf3a6943042cfdf3782e275185842
ha-matsumu <ha.matsumu@gmail.com>
2019-01-10 23:15:27 +0900

    modified router_test.js

--- test/src/routes/router_test.js
+++ test/src/routes/router_test.js
@@ -1,20 +1,26 @@
 const request = require("supertest");
 const app = require("../../../src/index");
 
-function runRouteTest(_url) {
+function runRouteTest(_url, _statusCode) {
   return request(app)
     .get(_url)
-    .expect(200);
+    .expect(_statusCode);
 }
 
 describe("GET /", () => {
-  it("top.ejsの表示", () => {
-    runRouteTest("/");
+  it("ステータスコード200になるはず", () => {
+    return runRouteTest("/", 200);
   });
 });
 
 describe("GET /quiz", () => {
-  it("quiz.ejsの表示", () => {
-    runRouteTest("/quiz");
+  it("ステータスコード200になるはず", () => {
+    return runRouteTest("/quiz", 200);
+  });
+});
+
+describe("GET /abc", () => {
+  it("ステータスコード404になるはず", () => {
+    return runRouteTest("/abc", 404);
   });
 });
