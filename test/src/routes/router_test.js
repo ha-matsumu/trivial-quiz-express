9a904a9dfcb929d655d052155795694c69ca50c6
ha-matsumu <ha.matsumu@gmail.com>
2019-01-10 14:59:29 +0900

    modified router_test.js

--- test/src/routes/router_test.js
+++ test/src/routes/router_test.js
@@ -1,18 +1,20 @@
 const request = require("supertest");
 const app = require("../../../src/index");
 
+function runRouteTest(_url, _callback) {
+  request(app)
+    .get(_url)
+    .expect(200, _callback);
+}
+
 describe("GET /", () => {
   it("top.ejsの表示", (done) => {
-    request(app)
-      .get("/")
-      .expect(200, done);
+   runRouteTest("/", done);
   });
 });
 
 describe("GET /quiz", () => {
   it("quiz.ejsの表示", (done) => {
-    request(app)
-      .get("/quiz")
-      .expect(200, done);
+    runRouteTest("/quiz", done);
   });
 });
